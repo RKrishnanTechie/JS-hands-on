896f00a2811959df86acd52539c4693474c8072e
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2024-02-26 19:57:24 +0000

    variables and datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3)  // we are using nodejs,  not browser
+//number
+// string
+// boolean
+// null --> a standalone value
+ 
+console.log(typeof undefined); // undefined only
+console.log(typeof null); // object
