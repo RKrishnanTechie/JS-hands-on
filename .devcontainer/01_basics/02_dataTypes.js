b9c1a519e984d08a587d09b694be8f1b0d807bcf
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2024-03-01 12:17:21 +0000

    type conversions and operations

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -1,9 +1,11 @@
 "use strict"; // treat all JS code as newer version
 // alert(3 + 3)  // we are using nodejs,  not browser
 //number
-// string
-// boolean
-// null --> a standalone value
+// string ""
+// boolean  true/false
+// null --> a standalone value   temperature = 0 is different from temperature = null
+//symbol --> uniqueness
+// object
  
 console.log(typeof undefined); // undefined only
 console.log(typeof null); // object
