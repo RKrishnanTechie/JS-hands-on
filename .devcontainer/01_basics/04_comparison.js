7450b56690137f21f3627decff805ef794e2bef1
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2024-03-01 14:36:09 +0000

    comparisons

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+// the equality == check work differently from  comparisons < > <= >= because the comparisons convert null to a number i.e., 0
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
