c878a81631e3671211440d098e5519709e99ef40
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2024-03-02 02:24:33 +0000

    updated datatype summary

--- .devcontainer/01_basics/datatypes_summary.js
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -35,4 +35,26 @@ const myFunction = function(){
 console.log(typeof myFunction);   
 // return typeof function is function , Array is object 
 
+
+
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//Stacks vs Heaps understanding 
+let myYoutubename = "rkyoutube"
+
+let anothername = myYoutubename; // copy of myYoutubename is given not the actual myYoutubename so any changes to anothername variable will its content but not myYoutubename value
+
+anothername = "akyoutube"
+
+console.log(myYoutubename);
+
+console.log(anothername);
+
+const userOne = {
+    mailId : "abc@gmail.com"
+}
+const userTwo = userOne;
+userTwo.mailId = "xyz@gmail.com" // changes the mailId property of both variables to xyz@gmail.com as they both reference the same object in the heap
+
+console.log(userOne);
+console.log(userTwo)
